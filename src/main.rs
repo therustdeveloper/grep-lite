a29a2617b184420b354ca6d10b5ee566343744c8
william <william@godeveloper.io>
2023-10-02 12:29:34 -0500

    Add reading arguments from command line and open files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let args = App::new("grep-lite")
@@ -11,21 +15,34 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
-
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books.
-What do we seek through millions of pages?";
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        if contains_substring.is_some() {
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
+}
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        if re.find(&line).is_some() {
             println!("{}", line)
         };
     }

17d34a22fc4d9e17297e73f051093da10b4c8ad8
william <william@godeveloper.io>
2023-10-02 11:01:05 -0500

    searching on a str

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let search_term = "picture";
+    let quote = "\
+Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books.
+What do we seek through millions of pages?";
+
+    let mut line_num: usize = 1;
+
+    for (i, line) in quote.lines().enumerate() {
+        if line.contains(search_term) {
+            println!("{}: {}", line_num, line);
+            line_num = i + 1;
+        }
+    }
 }
